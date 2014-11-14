98aaea7470937ca34957ef218e4b52a81e5f19a1
pmuilu <petteri@synble.com>
2014-11-14 15:02:36 +0200

    Added db_index to most fields that most likely are used for filtering.

--- django_messages/models.py
+++ django_messages/models.py
@@ -49,16 +49,16 @@ class Message(models.Model):
     """
     A private message from user to user
     """
-    subject = models.CharField(_("Subject"), max_length=120)
+    subject = models.CharField(_("Subject"), max_length=120, db_index=True)
     body = models.TextField(_("Body"))
-    sender = models.ForeignKey(AUTH_USER_MODEL, related_name='sent_messages', verbose_name=_("Sender"))
-    recipient = models.ForeignKey(AUTH_USER_MODEL, related_name='received_messages', null=True, blank=True, verbose_name=_("Recipient"))
-    parent_msg = models.ForeignKey('self', related_name='next_messages', null=True, blank=True, verbose_name=_("Parent message"))
-    sent_at = models.DateTimeField(_("sent at"), null=True, blank=True)
-    read_at = models.DateTimeField(_("read at"), null=True, blank=True)
-    replied_at = models.DateTimeField(_("replied at"), null=True, blank=True)
-    sender_deleted_at = models.DateTimeField(_("Sender deleted at"), null=True, blank=True)
-    recipient_deleted_at = models.DateTimeField(_("Recipient deleted at"), null=True, blank=True)
+    sender = models.ForeignKey(AUTH_USER_MODEL, related_name='sent_messages', verbose_name=_("Sender"), db_index=True)
+    recipient = models.ForeignKey(AUTH_USER_MODEL, related_name='received_messages', null=True, blank=True, verbose_name=_("Recipient"), db_index=True)
+    parent_msg = models.ForeignKey('self', related_name='next_messages', null=True, blank=True, verbose_name=_("Parent message"), db_index=True)
+    sent_at = models.DateTimeField(_("sent at"), null=True, blank=True, db_index=True)
+    read_at = models.DateTimeField(_("read at"), null=True, blank=True, db_index=True)
+    replied_at = models.DateTimeField(_("replied at"), null=True, blank=True, db_index=True)
+    sender_deleted_at = models.DateTimeField(_("Sender deleted at"), null=True, blank=True, db_index=True)
+    recipient_deleted_at = models.DateTimeField(_("Recipient deleted at"), null=True, blank=True, db_index=True)
 
     objects = MessageManager()
 
